1a96ce0362c0733fa36b0ffed76abafbfbe3a8dc
lsioctl <lsioctl@gmail.com>
2026-03-05 10:24:45 +0100

    chore: parse_string_litteral

--- src/lexer.rs
+++ src/lexer.rs
@@ -24,6 +24,28 @@ fn get_next_token_kind(c: Option<char>) -> Option<token::Kind> {
     }
 }
 
+fn parse_string_litteral(pk: &mut Peekable<Chars>, separator: char) -> Result<String, &'static str> {
+    let mut buf = String::new();
+
+    let mut is_finished = false;
+
+    for c in pk {
+        // can´t use pattern matching because separator is a runtime value
+        if c == separator {
+            is_finished = true;
+             break;
+        }
+
+        buf.push(c);
+    }
+
+    if is_finished {
+        Ok(buf)
+    } else {
+        Err("parsing error")
+    }
+}
+
 #[derive(Debug)]
 pub struct Lexer<'a> {
     // input: &'a str,
@@ -60,7 +82,29 @@ impl<'a> Lexer<'a> {
                 kind
             }
         } else {
-            
+            // if let Some(c) 
+            // match c {
+            //     if let Some(c
+            //         if ch.is_alphabetic() {
+            //             if !is_in_string_litteral {
+            //                 string_litteral_buffer = String::new();
+            //                 is_in_string_litteral = true;
+
+            //                 string_litteral_buffer.push(ch);
+            //             }
+
+            //             match self.chars.peek() {
+            //                 Some(ch) {
+            //                     match ch {
+
+            //                     }
+            //                 }
+            //             }
+            //         }
+            //     }
+            //     None => {
+                    
+            //     }
             // TODO dummy implementation for now
 
             let next_offset = offset + 1;
@@ -74,4 +118,33 @@ impl<'a> Lexer<'a> {
         }
     }
 
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_parse_string_litteral_ok() {
+        // I use raw sting litteral because I need double quotes
+        let mut pk = r#"abc"def"#.chars().peekable();
+
+        let sl = parse_string_litteral(&mut pk, '"').unwrap();
+
+        assert_eq!(sl, "abc");
+
+        assert_eq!(pk.next().unwrap(), 'd');
+    }
+
+    #[test]
+    fn test_parse_string_litteral_ko() {
+        // I use raw sting litteral because I need double quotes
+        let mut pk = r#"abcdef"#.chars().peekable();
+
+        let sl = parse_string_litteral(&mut pk, '"');
+
+        assert_eq!(sl, Err("parsing error"));
+
+        assert_eq!(pk.next(), None);
+    }
 }
